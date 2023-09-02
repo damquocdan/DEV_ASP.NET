862e3617cf6b8f1e20a5679dfd383dd2f13183a7
Đàm Quốc Dân <124369155+damquocdan@users.noreply.github.com>
2023-09-02 09:20:53 +0700

    Lesson02

---
+++ Lesson02/Controllers/AccountController.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Lesson02.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Lesson02.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Account> accounts = new List<Account>
+            {
+                new Account()
+                {
+                     Id = 1,Name="Hoàng Anh",
+                    Email="anh@gmail.com",
+                    Phone="0986456789",
+                    Address="Hà Nội",
+                    Avatar= Url.Content("~/Avatar/1.jpg"),
+                    Gender=1, Bio="My name is small",
+                    Birthday= new DateTime(1998,7,15)
+                },
+                new Account()
+                {
+                     Id = 2,Name="Hoàng Anh",
+                    Email="anh@gmail.com",
+                    Phone="0986456789",
+                    Address="Hà Nội",
+                    Avatar= Url.Content("~/Avatar/2.jpg"),
+                    Gender=1, Bio="My name is small",
+                    Birthday= new DateTime(1998,7,15)
+                },
+                new Account()
+                {
+                     Id = 3,Name="Hoàng Anh",
+                    Email="anh@gmail.com",
+                    Phone="0986456789",
+                    Address="Hà Nội",
+                    Avatar= Url.Content("~/Avatar/3.jpg"),
+                    Gender=1, Bio="My name is small",
+                    Birthday= new DateTime(1998,7,15)
+                },
+                new Account()
+                {
+                     Id = 4,Name="Hoàng Anh",
+                    Email="anh@gmail.com",
+                    Phone="0986456789",
+                    Address="Hà Nội",
+                    Avatar= Url.Content("~/Avatar/4.jpg"),
+                    Gender=1, Bio="My name is small",
+                    Birthday= new DateTime(1998,7,15)
+                },
+                new Account()
+                {
+                     Id = 5,Name="Hoàng Anh",
+                    Email="anh@gmail.com",
+                    Phone="0986456789",
+                    Address="Hà Nội",
+                    Avatar= Url.Content("~/Avatar/5.jpg"),
+                    Gender=1, Bio="My name is small",
+                    Birthday= new DateTime(1998,7,15)
+                },
+                new Account()
+                {
+                     Id = 6,Name="Hoàng Anh",
+                    Email="anh@gmail.com",
+                    Phone="0986456789",
+                    Address="Hà Nội",
+                    Avatar= Url.Content("~/Avatar/6.jpg"),
+                    Gender=1, Bio="My name is small",
+                    Birthday= new DateTime(1998,7,15)
+                },
+            };
+            ViewBag.Accounts = accounts;
+            return View();
+        }
+        public IActionResult Profile()
+        {
+            Account account=new Account()
+            {
+                Id = 6,
+                Name = "Hoàng Anh",
+                Email = "anh@gmail.com",
+                Phone = "0986456789",
+                Address = "Hà Nội",
+                Avatar = Url.Content("~/Avatar/6.jpg"),
+                Gender = 1,
+                Bio = "My name is small",
+                Birthday = new DateTime(1998, 7, 15)
+            };
+            ViewBag.account = account;
+            return View();
+        }
+        
+    }
+}
---
+++ Lesson02/Models/Account.cs
@@ -0,0 +1,16 @@
+﻿namespace Lesson02.Models
+{
+    public class Account
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Avatar { get; set; }
+        public string Address { get; set; }
+        public string Bio { get; set; }
+        public int Gender { get; set; }
+
+        public DateTime Birthday { get; set; }
+    }
+}
