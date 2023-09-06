6761e830d8d9cc8adad0065536c9475b1405f103
Đàm Quốc Dân <124369155+damquocdan@users.noreply.github.com>
2023-09-06 10:53:28 +0700

    6/9

--- Lesson02/Controllers/ProductController.cs
+++ Lesson02/Controllers/ProductController.cs
@@ -1,6 +1,8 @@
 ﻿using Lesson02.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Immutable;
+using static System.Net.Mime.MediaTypeNames;
+using System.Xml.Linq;
 
 namespace Lesson02.Controllers
 {
@@ -109,6 +111,82 @@ namespace Lesson02.Controllers
             return View();
             
 
+        }
+        public IActionResult Productct(int id)
+        {
+            List<Product> product = new List<Product>
+            {
+                new Product {
+                    Id = 1,
+                    Name = "Quần áo nam",
+                    Image="/Avatar/1.jpg",
+                    Price=1000000,
+                    SalePrice=699999,
+                    CategoryID=1,
+                    Description="Bộ đồ quần áo cho nam giới",
+                    Status=true,
+
+                },
+                new Product {
+                    Id = 2,
+                    Name = "Quần áo nữ",
+                    Image="/Avatar/2.jpg",
+                    Price=3000000,
+                    SalePrice=2490000,
+                    CategoryID=1,
+                    Description="Test",
+                    Status=true,
+
+                },
+                new Product {
+                    Id = 3,
+                    Name = "Quần áo cho trẻ nhỏ",
+                    Image="/Avatar/3.jpg",
+                    Price=100900,
+                    SalePrice=19900,
+                    CategoryID=1,
+                    Description="Test",
+                    Status=true,
+
+                },
+                new Product {
+                    Id = 4,
+                    Name = "Đồ gia dụng",
+                    Image="/Avatar/4.jpg",
+                    Price=100,
+                    SalePrice=100,
+                    CategoryID=4,
+                    Description="Test",
+                    Status=true,
+
+                },
+                new Product {
+                    Id = 5,
+                    Name = "Thực phẩm an toàn gia đình",
+                    Image="/Avatar/5.jpg",
+                    Price=100,
+                    SalePrice=100,
+                    CategoryID=5,
+                    Description="Test",
+                    Status=true,
+
+                },
+                new Product {
+                    Id = 6,
+                    Name = "Dụng cụ vườn nhà",
+                    Image="/Avatar/6.jpg",
+                    Price=100,
+                    SalePrice=100,
+                    CategoryID=6,
+                    Description="Test",
+                    Status=true,
+
+                }
+
+            };
+            Product product1 = product.FirstOrDefault(pd => pd.Id == id);
+            ViewBag.product = product1;
+            return View();
         }
        
     }
