b941a3f0897bb08fdedd4d2de642f1250f2cc0ff
Đàm Quốc Dân <124369155+damquocdan@users.noreply.github.com>
2023-09-07 15:31:12 +0700

    7/9

---
+++ Lesson03/ViewComponents/BookViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Lesson03.Models;
+namespace Lesson03.ViewComponents
+{
+    public class BookViewComponent : ViewComponent
+    {
+        protected Book book = new Book();
+        public IViewComponentResult Invoke()
+        {
+            var books = book.GetBookList();
+            return View(book);
+        }
+    }
+}
