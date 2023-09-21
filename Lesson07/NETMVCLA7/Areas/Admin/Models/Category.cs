e1d35a7e2085bdb9145252c0613ab5364acd6860
Đàm Quốc Dân <124369155+damquocdan@users.noreply.github.com>
2023-09-21 21:59:49 +0700

    21/9

---
+++ Lesson07/NETMVCLA7/Areas/Admin/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NETMVCLA7.Areas.Admin.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        [Display(Name = "Họ và tên")]
+        public string Name { get; set; }
+    }
+}
---
+++ Lesson07/NETMVCLA7/Areas/Admin/Models/Data.cs
@@ -0,0 +1,23 @@
+﻿namespace NETMVCLA7.Areas.Admin.Models
+{
+    public class Data
+    {
+        public static List<Category> categories = new List<Category>()
+        {
+
+            new Category() {
+                Id = 1,
+                Name = "Quần áo nam"
+            }
+        };
+        public static List<Category> GetCategories()
+        {
+            return categories;
+        }
+        public static Category? GetProductById(int Id)
+        {
+            var category = categories.FirstOrDefault(p => p.Id == Id);
+            return category;
+        }
+    }
+}
