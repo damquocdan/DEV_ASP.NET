6f0ef14eb3796a79c273699e6523d21c2b17a807
Đàm Quốc Dân <124369155+damquocdan@users.noreply.github.com>
2023-10-04 08:37:22 +0700

    410

--- Lesson09/Lesson09.Lap/Areas/Admins/Controllers/LoginController.cs
+++ Lesson09/Lesson09.Lap/Areas/Admins/Controllers/LoginController.cs
@@ -7,22 +7,27 @@ namespace Lesson09.Lap.Areas.Admins.Controllers
     [Area("Admins")]
     public class LoginController : Controller
     {
-        public object _context;
+        public DevXuongMocContext _context;
+
+        public LoginController(DevXuongMocContext context)
+        {
+            _context = context;
+        }
 
         [HttpGet]// get, hiển thị form để nhập dữ liệu
         public IActionResult Index()
-        {
+        { 
             return View();
         }
         [HttpPost] // POST -> khi submit form
         public IActionResult Index(Login model) {
-            if (ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
                 return View(model);// trả về trạng thái lỗi
             }
             // sẽ xử lý logic phần đăng nhập tại đây
             var pass = model.Password;
-            var dataLogin = _context.AdminUser.Where(x => x.Email.Equals(model.Email)&& x.Password.Equals(pass)).FirstOrDefault();
+            var dataLogin = _context.AdminUsers.Where(x => x.Email.Equals(model.Email)&& x.Password.Equals(pass)).FirstOrDefault();
             if (dataLogin != null)
             {
                 // Lưu session khi đăng nhập thành công
