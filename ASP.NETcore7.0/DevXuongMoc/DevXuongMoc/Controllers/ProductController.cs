66f6ec6c9f1f5380f5f67609ca4c0f2cc2483e4e
Đàm Quốc Dân <124369155+damquocdan@users.noreply.github.com>
2023-10-24 18:07:50 +0700

    24/10

---
+++ ASP.NETcore7.0/DevXuongMoc/DevXuongMoc/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using DevXuongMoc.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevXuongMoc.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly DevXuongMocSqlContext _context;
+        public ProductController(DevXuongMocSqlContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var data = await _context.Products.ToListAsync();
+
+            return View(data);
+        }
+    }
+}
